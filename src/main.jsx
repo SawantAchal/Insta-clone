83d0224b66bc1cab4e3e5c9084bc7ca972c4b2a1
SawantAchal <achals1718@gmail.com>
2024-01-30 15:51:19 +0530

    add style using global varible . change bg color and color for dark and light mode

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,31 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
+// 1. import `extendTheme` function
+import { extendTheme } from '@chakra-ui/react'
+import {mode} from  '@chakra-ui/theme-tools'
+
+const styles = {
+  global:(props) => ({
+    body:{
+      bg:mode("gray.100","#000")(props),
+      color:mode("gray.800","whitealpha.900")(props)
+    }
+  })
+}
+
+// 2. Add your color mode config
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+}
+
+// 3. extend the theme
+const theme = extendTheme({ config ,styles })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
