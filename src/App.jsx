adbfe6625833863b7449c7d7595faa775db34abf
SawantAchal <achals1718@gmail.com>
2024-01-30 17:14:27 +0530

    created page for login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/home/HomePage'
+import AuthPage from './pages/authPage/AuthPage'
 
 function App() {
   return (
     <>
-      Achal
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='/auth' element={<AuthPage />}/>
+      </Routes>
     </>
   )
 }
