456cfb728f5b31bd7bf71f81ecf849705f400c58
SawantAchal <achals1718@gmail.com>
2024-01-31 13:49:04 +0530

    Pass the props for FeedPost component

---
+++ src/components/feedPosts/FeedPosts.jsx
@@ -0,0 +1,16 @@
+import { Container } from '@chakra-ui/react'
+import React from 'react'
+import FeedPost from './FeedPost'
+
+const FeedPosts = () => {
+  return (
+    <Container maxW={'container.sm'} py={10} px={2}>
+        <FeedPost img='/img1.png' username='achal sawant' avatar='/img1.png'/>
+        <FeedPost img='/img2.png' username='amol dhawle' avatar='/img2.png'/>
+        <FeedPost img='/img3.png' username='Bubu' avatar='/img3.png'/>
+        <FeedPost img='/img4.png' username='Dudu' avatar='/img4.png'/>
+    </Container>
+  )
+}
+
+export default FeedPosts
