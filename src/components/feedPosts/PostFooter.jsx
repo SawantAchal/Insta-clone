5f8c9739ce89db4372b83acc4326873210bea710
SawantAchal <achals1718@gmail.com>
2024-01-31 19:47:03 +0530

    destructed isProfilePage pros coming from profilepost component to hide name view all text in modal .

--- src/components/feedPosts/PostFooter.jsx
+++ src/components/feedPosts/PostFooter.jsx
@@ -2,7 +2,7 @@ import { Box, Button, Flex, Input, InputGroup, InputRightElement, Text } from '@
 import React, { useState } from 'react'
 import {CommentLogo, NotificationsLogo ,UnlikeLogo} from '../../assets/constant'
 
-const PostFooter = ({username}) => {
+const PostFooter = ({username ,isProfilePage}) => {
   const [liked , setLiked] = useState(false)
   const [likes , setLikes] = useState(1000)
 
@@ -18,7 +18,7 @@ const PostFooter = ({username}) => {
   }
 
   return (
-    <Box my={10}>
+    <Box my={10} marginTop={'auto'}>
       <Flex alignItems={'center'} gap={4} w={'full'} pt={0} mb={2} mt={4}>
         <Box onClick={handleLike} cursor={'pointer'} fontSize={18}>
           {
@@ -32,15 +32,21 @@ const PostFooter = ({username}) => {
       <Text  fontWeight={600} fontSize={'sm'}>
         {likes} likes
       </Text>
-      <Text fontWeight={700} fontSize={'sm'}>
-        {username}{' '}
-        <Text as='span' fontWeight={400}>
-          Feeling good
-        </Text>
-      </Text>
-      <Text fontSize={'sm'} color={'gray'}>
-        View all 1,000 comments
-      </Text>
+      {
+        !isProfilePage && (
+          <>
+            <Text fontWeight={700} fontSize={'sm'}>
+              {username}{' '}
+              <Text as='span' fontWeight={400}>
+                Feeling good
+              </Text>
+            </Text>
+            <Text fontSize={'sm'} color={'gray'}>
+              View all 1,000 comments
+            </Text>
+          </>
+        )
+      }
       <Flex alignItems={'center'} gap={2} justifyContent={'space-between'} w={'full'}>
           <InputGroup>
             <Input variant={'flushed'} placeholder={'Add a comment...'} fontSize={14}/>
