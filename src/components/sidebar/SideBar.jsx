1519f30eb16c7718295ebce6325e992b4f1b2dcc
SawantAchal <achals1718@gmail.com>
2024-02-01 16:56:26 +0530

    Add logout function

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,9 +1,10 @@
-import { Avatar, Box, Flex, Link, Tooltip } from '@chakra-ui/react'
+import { Avatar, Box, Button, Flex, Link, Tooltip } from '@chakra-ui/react'
 import React from 'react'
 import { Link as RouterLink } from 'react-router-dom'
 import { CreatePostLogo, InstagramLogo, InstagramMobileLogo, NotificationsLogo, SearchLogo } from '../../assets/constant'
 import {AiFillHome} from 'react-icons/ai'
 import {BiLogOut} from 'react-icons/bi'
+import useLogout from '../../hooks/useLogout'
 
 const SideBar = () => {
   const siderBarItems = [
@@ -31,6 +32,7 @@ const SideBar = () => {
     }
   ]
 
+  const {handleLogout , isLoggingOut , error} = useLogout()
   return (
     <Box height={'100vh'} borderRadius={'1px solid'} borderColor={'whiteAlpha.300'} py={8} position={'sticky'} top={0} left={0} px={{base:2 , md:4}}>
       <Flex direction={'column'} gap={10} w={'full'} height={'full'}>
@@ -58,12 +60,12 @@ const SideBar = () => {
           }
         </Flex>
         <Tooltip hasArrow label={'Logout'} placement='right' ml={1} openDelay={500} display={{base:'block' , md:'none' }}>
-          <Link display={'flex'} to={'/auth'} as={RouterLink} alignItems={'center'} gap={4} _hover={{bg:'whiteAlpha.400'}} borderRadius={6} p={2} w={{base:10 , md:'full'}} justifyContent={{base:'center' , md:'flex-start'}} mt={'auto'}>
+          <Flex onClick={handleLogout} alignItems={'center'} gap={4} _hover={{bg:'whiteAlpha.400'}} borderRadius={6} p={2} w={{base:10 , md:'full'}} justifyContent={{base:'center' , md:'flex-start'}} mt={'auto'}>
             <BiLogOut size={25}/>
-            <Box display={{base:'none', md:'block'}}>
+            <Button variant={'ghost'} display={{base:'none', md:'block'}} _hover={{bg:'transparent'}} isLoading={isLoggingOut}>
               Logout
-            </Box>
-          </Link>
+            </Button>
+          </Flex>
         </Tooltip>
       </Flex>
     </Box>
