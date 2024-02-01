2003664347307d200eecdf95e5a1fb3cf6a33742
SawantAchal <achals1718@gmail.com>
2024-02-01 16:00:02 +0530

    to avoid see sidebar on auth page add condition for that

--- src/Layouts/pageLayouts/PageLayout.jsx
+++ src/Layouts/pageLayouts/PageLayout.jsx
@@ -1,20 +1,36 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, Flex, Spinner } from '@chakra-ui/react'
 import React from 'react'
 import SideBar from '../../components/sidebar/SideBar'
 import { useLocation } from 'react-router-dom'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from '../../firebase/firebase'
+import Navbar from '../../components/navbar/Navbar'
 
 const PageLayout = ({children}) => {
     const {pathname} = useLocation()
+    const [user, loading, error] = useAuthState(auth);
+    const canRenderSidebar = pathname !== '/auth' && user;
+    const canRenderNavbar = !user && !loading && pathname !== '/auth';
+
+    const checkingUserIsAuth = !user && loading
+    if (checkingUserIsAuth) return <PageLayoutSpinner />
+    
   return (
-    <Flex>
+    <Flex flexDir={canRenderNavbar ? "column" : "row"}>
         {/* side bar on left */}
-        {pathname !== '/auth' ? (
+        {/* {pathname !== '/auth' ? ( */}
+        {/* sidebar */}
+        {canRenderSidebar ? (
             <Box w={{base:'70px' , md:'240px'}}>
                 <SideBar/>
             </Box>
         ):null}
+        {/* navbar */}
+        {
+            canRenderNavbar ? <Navbar /> : null
+        }
         {/* the page content in the right */}
-        <Box flex={1} w={{base:'calc(100% - 70px)', md:'calc(100%- 240px)'}}>
+        <Box flex={1} w={{base:'calc(100% - 70px)', md:'calc(100%- 240px)'}} mx={'auto'}>
             {children}
         </Box>
     </Flex>
@@ -22,3 +38,13 @@ const PageLayout = ({children}) => {
 }
 
 export default PageLayout
+
+
+const PageLayoutSpinner = () => {
+  return (
+    <Flex flexDir={'column'} h={'100vh'} alignItems={'center'} justifyContent={'center'}>
+        <Spinner size={'xl'}/>
+    </Flex>
+  )
+}
+
