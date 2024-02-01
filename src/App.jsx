b6ea9ad67a105b9f1e94a8589512bf4216c42784
SawantAchal <achals1718@gmail.com>
2024-02-01 15:58:22 +0530

    checking user is login or not

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/home/HomePage'
 import AuthPage from './pages/authPage/AuthPage'
 import PageLayout from './Layouts/pageLayouts/PageLayout'
+import ProfilePage from './pages/profilePage/ProfilePage'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from './firebase/firebase'
 
 function App() {
+  // const authUser = useAuthStore(state => state.user)
+  const [authUser] = useAuthState(auth)
   return (
     <PageLayout>
       <Routes>
-        <Route path='/' element={<HomePage />}/>
-        <Route path='/auth' element={<AuthPage />}/>
+        <Route path='/' element={authUser ? <HomePage /> : <Navigate to='/auth' />}/>
+        <Route path='/auth' element={!authUser ? <AuthPage /> : <Navigate to='/' />}/>
+        <Route path='/:username' element={<ProfilePage />}/>
       </Routes>
     </PageLayout>
   )
