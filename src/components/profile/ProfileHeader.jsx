6621fcc9c1a4eebacf7b3069ba644a218d5ae1cd
SawantAchal <achals1718@gmail.com>
2024-02-03 13:58:22 +0530

    Add useFollowAndUnfollow hook and function on follow button

--- src/components/profile/ProfileHeader.jsx
+++ src/components/profile/ProfileHeader.jsx
@@ -1,42 +1,80 @@
-import { Avatar, AvatarGroup, Button, Flex, Text, VStack } from '@chakra-ui/react'
+import { Avatar, AvatarGroup, Button, Flex, Text, VStack, useDisclosure } from '@chakra-ui/react'
 import React from 'react'
+import useUserProfileStore from '../../store/userProfileStore'
+import useAuthStore from '../../store/authStore'
+import EditProfile from './EditProfile'
+import useFollowUnfollowUser from '../../hooks/useFollowUnfollowUser'
 
 const ProfileHeader = () => {
+    const {userProfile} = useUserProfileStore()
+    const authUser = useAuthStore(state => state.user);
+    //for follow or unfollow
+    const {isUpdating , isFollowing ,handleFollowUser}=useFollowUnfollowUser(userProfile?.uid)
+    const visitingOwnProfileAndAuth = authUser && authUser.username === userProfile.username;
+    const visitingAnotherProfileAndAuth = authUser && authUser.username !== userProfile.username;
+    // for modal
+    const {isOpen , onOpen , onClose} = useDisclosure()
   return (
     <Flex gap={{base:4,sm:10}} py={10} direction={{base:'column' , sm:'row'}}>
         <AvatarGroup size={{base:'xl', md:'2xl'}} justifyContent={'center'} alignSelf={'flex-start'} mx={'auto'}>
-            <Avatar name='As a Programmer' src='/profilepic.png' alt='As a Programmer'/>
+            <Avatar  src={userProfile.profilePicURL} alt='As a Programmer'/>
         </AvatarGroup>
         <VStack alignItems={'start'} gap={2} mx={'auto'} flex={1}>
             <Flex gap={4} direction={{base:'column' , sm:'row'}} justifyContent={{base:'center', sm:'flex-start'}} alignItems={'center'} w={'full'}>
                 <Text fontSize={{base:'small' ,md:'large'}}>
-                    asaprogrammmer
+                    {userProfile.username}
                 </Text>
-                <Flex gap={4} alignItems={'center'} justifyContent={'center'}>
-                    <Button bg={'white'} color={'black'} _hover={{bg:'whiteAlpha.800'}} size={{base:'xs' , md:'sm'}}>
-                        Edit Profile
-                    </Button>
-                </Flex>
+                {
+                    visitingOwnProfileAndAuth && (
+                        <Flex gap={4} alignItems={'center'} justifyContent={'center'}>
+                            <Button bg={'white'} color={'black'} _hover={{bg:'whiteAlpha.800'}} size={{base:'xs' , md:'sm'}} onClick={onOpen}>
+                                Edit Profile
+                            </Button>
+                        </Flex>
+                    )
+                }
+                {
+                    visitingAnotherProfileAndAuth && (
+                        <Flex gap={4} alignItems={'center'} justifyContent={'center'}>
+                            <Button bg={'blue.500'} color={'white'} _hover={{bg:'blue.600'}} size={{base:'xs' , md:'sm'}} onClick={handleFollowUser} isLoading={isUpdating}>
+                                {
+                                    isFollowing ? "Unfollow" :"Follow"
+                                }
+                            </Button>
+                        </Flex>
+                    )
+                }
+
             </Flex>
             <Flex alignItems={'center'} gap={{base:2 , sm:4}}>
                 <Text fontSize={{base:'x-small' , md:'small'}}>
-                    <Text as={'span'} fontWeight={'bold'} mr={1}>4</Text>
+                    <Text as={'span'} fontWeight={'bold'} mr={1}>
+                        {userProfile.posts.length}
+                    </Text>
                     Posts
                 </Text>
                 <Text fontSize={{base:'x-small' , md:'small'}}>
-                    <Text as={'span'} fontWeight={'bold'} mr={1}>149</Text>
+                    <Text as={'span'} fontWeight={'bold'} mr={1}>
+                        {userProfile.followers.length}
+                    </Text>
                     Followers
                 </Text>
                 <Text fontSize={{base:'x-small' , md:'small'}}>
-                    <Text as={'span'} fontWeight={'bold'} mr={1}>134</Text>
+                    <Text as={'span'} fontWeight={'bold'} mr={1}>
+                        {userProfile.following.length}
+                    </Text>
                     Following
                 </Text>
             </Flex>
             <Flex alignItems={'center'} gap={4}>
-                <Text fontSize={'sm'} fontWeight={'bold'}> AS a Programmer</Text>
+                <Text fontSize={'sm'} fontWeight={'bold'}>{userProfile.fullName}</Text>
             </Flex>
-            <Text fontSize={'sm'}> ASjsdfkjsfhsdfjdkf dfhskdfh a Programmer</Text>
+            <Text fontSize={'sm'}>{userProfile.bio}</Text>
         </VStack>
+        {/* for edit profile modal */}
+        {
+            isOpen && <EditProfile isOpen={isOpen} onClose={onClose}/>
+        }
     </Flex>
   )
 }
