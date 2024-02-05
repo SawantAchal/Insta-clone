33ed3821f18c9769c1657cfe9a7b9fae7b1eeaec
SawantAchal <achals1718@gmail.com>
2024-02-05 20:05:07 +0530

    remove unnecessary code

--- src/components/authForm/SignUp.jsx
+++ src/components/authForm/SignUp.jsx
@@ -6,17 +6,17 @@ import useSignUpWithEmailAndPassword from '../../hooks/useSignUpWithEmailAndPass
 const SignUp = () => {
     const [inputs , setInputs] = useState({email : '' , password:'' ,username:'' , fullName:''})
     const [showPassword , setShowPassword] = useState(false);
-    const {user,loading,error,signup} = useSignUpWithEmailAndPassword()
+    const {loading,error,signup} = useSignUpWithEmailAndPassword()
 
   return (
     <>
         <Input placeholder='Email' fontSize={14} type='email' value={inputs.email} onChange={(e) => setInputs({...inputs,email:e.target.value})} size={'sm'}/>
         <Input placeholder='username' fontSize={14} type='text' value={inputs.username} onChange={(e) => setInputs({...inputs,username:e.target.value})} size={'sm'}/>
-        <Input placeholder='fullname' fontSize={14} type='email' value={inputs.fullName} onChange={(e) => setInputs({...inputs,fullName:e.target.value})} size={'sm'}/>
+        <Input placeholder='fullname' fontSize={14} type='text' value={inputs.fullName} onChange={(e) => setInputs({...inputs,fullName:e.target.value})} size={'sm'}/>
         <InputGroup>
             <Input placeholder='Password' fontSize={14} type={showPassword ? 'text' : 'password'} value={inputs.password} onChange={(e) => setInputs({...inputs,password:e.target.value})} size={'sm'} />
             <InputRightElement h={'full'}>
-                <Button variant={'ghost'} size={'sm'} onClick={() => setShowPassword (!showPassword)}>
+                <Button variant={'ghost'} size={'sm'} onClick={() => setShowPassword(!showPassword)}>
                     {
                         showPassword ? <ViewIcon/> : <ViewOffIcon />
                     }
