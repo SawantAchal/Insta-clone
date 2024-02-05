ebf2e5596f9f1d0354987538b5adbd799fd0fadb
SawantAchal <achals1718@gmail.com>
2024-02-05 20:30:28 +0530

    add style

--- src/components/profile/ProfileHeader.jsx
+++ src/components/profile/ProfileHeader.jsx
@@ -16,7 +16,7 @@ const ProfileHeader = () => {
     const {isOpen , onOpen , onClose} = useDisclosure()
   return (
     <Flex gap={{base:4,sm:10}} py={10} direction={{base:'column' , sm:'row'}}>
-        <AvatarGroup size={{base:'xl', md:'2xl'}} justifyContent={'center'} alignSelf={'flex-start'} mx={'auto'}>
+        <AvatarGroup size={{base:'xl', md:'2xl'}} justifySelf={'center'} alignSelf={'flex-start'} mx={'auto'}>
             <Avatar  src={userProfile.profilePicURL} alt='As a Programmer'/>
         </AvatarGroup>
         <VStack alignItems={'start'} gap={2} mx={'auto'} flex={1}>
@@ -38,7 +38,7 @@ const ProfileHeader = () => {
                         <Flex gap={4} alignItems={'center'} justifyContent={'center'}>
                             <Button bg={'blue.500'} color={'white'} _hover={{bg:'blue.600'}} size={{base:'xs' , md:'sm'}} onClick={handleFollowUser} isLoading={isUpdating}>
                                 {
-                                    isFollowing ? "Unfollow" :"Follow"
+                                    isFollowing ? "Unfollow" : "Follow"
                                 }
                             </Button>
                         </Flex>
@@ -47,19 +47,19 @@ const ProfileHeader = () => {
 
             </Flex>
             <Flex alignItems={'center'} gap={{base:2 , sm:4}}>
-                <Text fontSize={{base:'x-small' , md:'small'}}>
+                <Text fontSize={{base:'xs' , md:'sm'}}>
                     <Text as={'span'} fontWeight={'bold'} mr={1}>
                         {userProfile.posts.length}
                     </Text>
                     Posts
                 </Text>
-                <Text fontSize={{base:'x-small' , md:'small'}}>
+                <Text fontSize={{base:'xs' , md:'sm'}}>
                     <Text as={'span'} fontWeight={'bold'} mr={1}>
                         {userProfile.followers.length}
                     </Text>
                     Followers
                 </Text>
-                <Text fontSize={{base:'x-small' , md:'small'}}>
+                <Text fontSize={{base:'xs' , md:'sm'}}>
                     <Text as={'span'} fontWeight={'bold'} mr={1}>
                         {userProfile.following.length}
                     </Text>
