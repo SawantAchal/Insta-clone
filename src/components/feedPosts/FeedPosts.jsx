08a46471dce4695039e508b850af71f504cd0bf6
SawantAchal <achals1718@gmail.com>
2024-02-05 20:13:36 +0530

    add condition if there is no frd then show msg

--- src/components/feedPosts/FeedPosts.jsx
+++ src/components/feedPosts/FeedPosts.jsx
@@ -1,21 +1,15 @@
-import { Box, Container, Flex, Skeleton, SkeletonCircle, VStack } from '@chakra-ui/react'
+import { Box, Container, Flex, Skeleton, SkeletonCircle, Text, VStack } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 import FeedPost from './FeedPost'
+import useGetFeedPosts from '../../hooks/useGetFeedPosts'
 
 const FeedPosts = () => {
-  const [isLoading , setIsLoading] = useState(true)
-
-  //set time for loading skeleton
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false)
-    }, 2000);
-  }, [])
+  const {isLoading ,posts} = useGetFeedPosts()
 
   return (
     <Container maxW={'container.sm'} py={10} px={2}>
       {
-        isLoading && [0,1,2,3].map((_,index) => (
+        isLoading && [0,1,2].map((_,index) => (
           <VStack key={index} gap={4} alignItems={'flex-start'} mb={10}>
             <Flex gap={2}>
               <SkeletonCircle size={10} />
@@ -25,18 +19,21 @@ const FeedPosts = () => {
               </VStack>
             </Flex>
             <Skeleton w={'full'}>
-              <Box h={'500px'}>contents wrapped</Box>
+              <Box h={'400px'}>contents wrapped</Box>
             </Skeleton>
           </VStack>
         ))
       }
       {
-        !isLoading && (
+        !isLoading && posts.length > 0 && posts.map((post) => <FeedPost key={post.id} post={post} />)
+      }
+      {
+        !isLoading && posts.length === 0 && (
           <>
-            <FeedPost img='/img1.png' username='achal sawant' avatar='/img1.png'/>
-            <FeedPost img='/img2.png' username='amol dhawle' avatar='/img2.png'/>
-            <FeedPost img='/img3.png' username='Bubu' avatar='/img3.png'/>
-            <FeedPost img='/img4.png' username='Dudu' avatar='/img4.png'/>
+            <Text fontSize={'md'} color={'red.400'}>
+              Dayuum. Looks like you don&apos;t have any friends.
+            </Text>
+            <Text color={"red.400"}>Stop coding and go make some!!</Text>
           </>
         )
       }
