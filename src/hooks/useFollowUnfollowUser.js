ea65d79c71248b21d02da63ce5103431cbb0ffa8
SawantAchal <achals1718@gmail.com>
2024-02-05 21:28:00 +0530

    add authentication

--- src/hooks/useFollowUnfollowUser.js
+++ src/hooks/useFollowUnfollowUser.js
@@ -8,13 +8,13 @@ import { firestore } from '../firebase/firebase'
 const useFollowUnfollowUser = (userId) => {
     const [isUpdating , setIsUpadating] = useState(false)
     const [isFollowing , setIsFollowing] = useState(false)
-    // const {authUser , setAuthUser} = useAuthStore()
     const authUser = useAuthStore((state) => state.user)
     const setAuthUser = useAuthStore((state) => state.setUser)
     const {userProfile , setUserProfile} = useUserProfileStore()
     const showToast = useShowToast()
 
     const handleFollowUser = async () => {
+        setIsUpadating(true)
         try {
             const currentUserRef = doc(firestore,"users",authUser.uid)
             const userToFollowOrUnfollowRef = doc(firestore,"users",userId)
@@ -33,16 +33,16 @@ const useFollowUnfollowUser = (userId) => {
                 //unfollow the is user
                 setAuthUser({
                     ...authUser,
-                    following:authUser.following.filter(uid => uid !== userId)
+                    following:authUser.following.filter((uid) => uid !== userId)
                 })
                 if(userProfile)
                     setUserProfile({
                         ...userProfile,
-                        followers:userProfile.followers.filter(uid => uid !== authUser.uid)
+                        followers:userProfile.followers.filter((uid) => uid !== authUser.uid)
                     })
                 localStorage.setItem("user-info",JSON.stringify({
                     ...authUser,
-                    following:authUser.following.filter(uid => uid !== userId)
+                    following:authUser.following.filter((uid) => uid !== userId)
                 }))
                 setIsFollowing(false)
             }else{
